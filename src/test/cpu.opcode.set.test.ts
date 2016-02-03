c5c8769acbb29506b0a52664f5662761158885ce
Cy Brown <cy.brown59@gmail.com>
2016-02-03 18:15:00 +0100

    Refactor test

--- src/test/cpu.opcode.set.test.ts
+++ src/test/cpu.opcode.set.test.ts
@@ -1,7 +1,7 @@
 import * as assert from 'assert';
 import {CPU} from '../main/cpu';
 
-describe ('CPU', () => {
+describe ('CPU Opcode SET (0x6XNN)', () => {
 
     let cpu: CPU;
 
@@ -9,16 +9,13 @@ describe ('CPU', () => {
         cpu = new CPU();
     });
 
-    describe ('Opcode SET (0x6XNN)', () => {
+    it ('should set the register V1 with a value', () => {
+        cpu.execute(0x6122);
+        assert.equal(cpu.V1, 34);
+    });
 
-        it ('should set the register V1 with a value', () => {
-            cpu.execute(0x6122);
-            assert.equal(cpu.V1, 34);
-        });
-
-        it ('should set the register VA with a value', () => {
-            cpu.execute(0x6AFF);
-            assert.equal(cpu.VA, 255);
-        });
+    it ('should set the register VA with a value', () => {
+        cpu.execute(0x6AFF);
+        assert.equal(cpu.VA, 255);
     });
 });
