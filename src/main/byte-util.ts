02956485d04dac434adce2a99bfbd7a1b6811d87
Cy Brown <cy.brown59@gmail.com>
2016-02-04 19:42:03 +0100

    Ajout commentaires sur byte-util

--- src/main/byte-util.ts
+++ src/main/byte-util.ts
@@ -1,40 +1,40 @@
 /**
- * Renvoie les 4 premiers bits
+ * Renvoie les 4 premiers bits: 0x000N
  */
 export function nibble0(value: number): number {
     return value & 0xF;
 }
 
 /**
- * Renvoie les 4 bits en seconde position
+ * Renvoie les 4 bits en seconde position: 0x00N0
  */
 export function nibble1(value: number): number {
     return (value & 0xF0) >> 4;
 }
 
 /**
- * Renvoie les 4 avant dernier bits
+ * Renvoie les 4 avant dernier bits: 0x0N00
  */
 export function nibble2(value: number): number {
     return (value & 0xF00) >> 8;
 }
 
 /**
- * Renvoie les 4 derniers bits
+ * Renvoie les 4 derniers bits: 0xN000
  */
 export function nibble3(value: number): number {
     return (value & 0xF000) >> 12;
 }
 
 /**
- * Renvoie l'octet de poids faible
+ * Renvoie l'octet de poids faible: 0x00NN
  */
 export function byte0(value: number): number {
     return value & 0xFF;
 }
 
 /**
- * Renvoie l'octet de poids fort
+ * Renvoie l'octet de poids fort: 0xNN00
  */
 export function byte1(value: number): number {
     return (value & 0xFF00) >> 8;
