f70c02bb42e0cb613ec377981504a3da8d0bb5c0
Cy Brown <cy.brown59@gmail.com>
2016-02-04 18:55:00 +0100

    Renommage opcode set => ld (load)

--- src/test/cpu.opcode.ld.test.ts
+++ src/test/cpu.opcode.ld.test.ts
@@ -1,7 +1,7 @@
 import * as assert from 'assert';
 import {CPU} from '../main/cpu';
 
-describe ('CPU Opcode SET (0x6XNN)', () => {
+describe ('CPU Opcode LD (0x6XNN)', () => {
 
     let cpu: CPU;
 
@@ -9,12 +9,12 @@ describe ('CPU Opcode SET (0x6XNN)', () => {
         cpu = new CPU();
     });
 
-    it ('should set the register V1 with a value', () => {
+    it ('should load the value 34 in the V1 register', () => {
         cpu.execute(0x6122);
         assert.equal(cpu.V1, 34);
     });
 
-    it ('should set the register VA with a value', () => {
+    it ('should load the value 0xFF in the VA register', () => {
         cpu.execute(0x6AFF);
         assert.equal(cpu.VA, 255);
     });
